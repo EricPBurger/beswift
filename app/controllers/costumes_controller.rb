08c00f25d064039581899b7698b218cda1500e78
James Trinder <jamesanthonyt@live.co.uk>
2020-02-04 19:12:00 +0000

    Created the costumes controller

---
+++ app/controllers/costumes_controller.rb
@@ -0,0 +1,42 @@
+class CostumesController < ApplicationController
+  def index
+    @costumes = Costume.all
+  end
+
+  def show
+    @costume = Costume.find(params[:id])
+  end
+
+  def new
+    @costume = Costume.new
+  end
+
+  def create
+    @costume = Costume.new(costume_params)
+    if @costume.save
+      redirect_to costume_path(@costume)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @costume = Costume.find(params[:id])
+  end
+
+  def update
+    @costume = Costume.find(params[:id])
+    if @costume.update(costume_params)
+      redirect_to costume_path(@costume)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:costume_name, :origin, :price, :size,
+                                    :desc_short,  :desc_long, :availability)
+  end
+end
