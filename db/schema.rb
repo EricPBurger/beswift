a3880ec00f42024def13defbc08e239763f70da6
Jamie Newton <newtonjameshenry@gmail.com>
2020-02-04 19:01:45 +0000

    changed navbar and fixed it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_02_01_161146) do
 
   # These are extensions that must be enabled in order to support this database
@@ -79,7 +78,6 @@ ActiveRecord::Schema.define(version: 2020_02_01_161146) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "bookings", "costumes"
   add_foreign_key "bookings", "users"
